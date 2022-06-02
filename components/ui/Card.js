1fb22b8169cde64bb11f8cb489269ec9f969ab73
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-02 10:02:30 +0800

    feat: dimension API

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,25 +1,27 @@
-import { View, StyleSheet } from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
 
 import Colors from '../../constants/colors';
 
-function Card({ children }) {
+function Card({children}) {
   return <View style={styles.card}>{children}</View>;
 }
 
 export default Card;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   card: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 4,
     shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowRadius: 6,
     shadowOpacity: 0.25,
   },
